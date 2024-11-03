5ff1f1e4be341b3219e6f07b0b98fb3a3fe21ff0
sai karthik ketha <69042752+saiisback@users.noreply.github.com>
2024-11-03 15:01:20 +0000

    update 0.4

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Hero() {
   return (
     <div className="flex items-center justify-center overflow-hidden m-10 md:m-20 lg:m-36">
-      <h1 className="text-center font-bold text-shadow-lg text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
+      <h1 className="text-center font-bold text-shadow-lg text-6xl sm:text-8xl md:text-8xl lg:text-8xl">
         <span className="text-[rgb(255,248,220)]">S</span>
         <span className="text-[rgb(255,0,0)]">O</span>
         <span className="text-[rgb(165,42,42)]">U</span>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav className="w-full p-2 sm:p-4 mt-4 sm:mt-10 flex justify-center">
+    <nav className="w-full p-2 sm:p-4 mt-4 sm:mt-28 flex justify-center">
       <div className="border bg-[#fcd5b5] backdrop-blur-3xl border-[#e7a78a] rounded-full px-4 py-1 sm:px-6 sm:py-2">
-        <ul className="flex space-x-4 text-sm sm:space-x-6 sm:text-xl">
+        <ul className="flex space-x-4 text-sm sm:space-x-6 sm:text-3xl">
           <li>
             <a href="#" className="text-[#9c5f4d] hover:text-[#ce7d5d]">Home</a>
           </li>
