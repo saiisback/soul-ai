b59c1e54db2d48f1abd662b1e31caa50c3714ccb
sai karthik ketha <69042752+saiisback@users.noreply.github.com>
2024-11-03 14:53:24 +0000

    update 0.3

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Hero() {
   return (
-    <div className="flex m-36 overflow-hidden items-center justify-center text-9xl font-bold text-shadow-lg">
-      <h1>
-        <span style={{ color: "rgb(255, 248, 220)" }}>S</span>
-        <span style={{ color: "rgb(222, 49, 99)" }}>O</span>
-        <span style={{ color: "rgb(165, 42, 42)" }}>U</span>
-        <span style={{ color: "rgb(255, 228, 121)" }}>L</span>
+    <div className="flex items-center justify-center overflow-hidden m-10 md:m-20 lg:m-36">
+      <h1 className="text-center font-bold text-shadow-lg text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
+        <span className="text-[rgb(255,248,220)]">S</span>
+        <span className="text-[rgb(255,0,0)]">O</span>
+        <span className="text-[rgb(165,42,42)]">U</span>
+        <span className="text-[rgb(255,228,121)]">L</span>
       </h1>
     </div>
   );
